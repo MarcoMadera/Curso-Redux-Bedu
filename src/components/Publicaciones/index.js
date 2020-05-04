a708b8cdba17f17cbfd70cf98eef81f4ba636405
Marco Madera <marcoseguro2@gmail.com>
2020-05-03 23:35:19 -0700

    Clase 39: Mostrar componentes dinámicamente

--- src/components/Publicaciones/index.js
+++ src/components/Publicaciones/index.js
@@ -2,11 +2,12 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Spinner from '../General/Spinner'
 import Fatal from '../General/Fatal'
+import Comentarios from '../Comentarios'
 import * as usuariosActions from '../../actions/usuariosActions';
 import * as publicacionesActions from '../../actions/publicacionesActions';
 
 const { traerTodos: usuariosTraerTodos } = usuariosActions;
-const { traerPorUsuario: publicacionesTraerPorUsuario, abrirCerrar } = publicacionesActions
+const { traerPorUsuario: publicacionesTraerPorUsuario, abrirCerrar, traerComentarios } = publicacionesActions
 
 class Publicaciones extends Component {
     async componentDidMount() {
@@ -82,7 +83,7 @@ class Publicaciones extends Component {
             <div 
               className='pub_titulo'
               key = { publicacion.id }
-              onClick = { ()=>this.props.abrirCerrar(pub_key, com_key) }
+              onClick = { ()=>this.mostrarComentarios(pub_key, com_key, publicacion.comentarios) }
             >
               <h2>
                 { publicacion.title }
@@ -91,11 +92,16 @@ class Publicaciones extends Component {
                 { publicacion.body }
               </h3>
               {
-                  (publicacion.abierto) ? 'abierto' : 'cerrado'
+                  (publicacion.abierto) ? <Comentarios /> : 'cerrado'
               }
             </div>
         ))
     );
+
+    mostrarComentarios = (pub_key, com_key, comentarios) => {
+        this.props.abrirCerrar(pub_key, com_key)
+        this.props.traerComentarios(pub_key, com_key)
+    }
 
     render() {
         console.log(this.props);
@@ -118,7 +124,8 @@ const mapStateToProps = ({usuariosReducer, publicacionesReducer}) => {
 const mapDispatchToProps = {
     usuariosTraerTodos,
     publicacionesTraerPorUsuario,
-    abrirCerrar
+    abrirCerrar,
+    traerComentarios
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(Publicaciones) 
